176ca9b796036e08f0e0b3597cb133aee479a038
OctagonalStar <76486554+OctagonalStar@users.noreply.github.com>
2025-10-15 23:00:31 +0800

    independentify change model
    Signed-off-by: OctagonalStar <76486554+OctagonalStar@users.noreply.github.com>

---
+++ lib/change_notifier_models.dart
@@ -0,0 +1,67 @@
+import 'dart:convert';
+import 'dart:io';
+import 'package:arabic_learning/statics_var.dart';
+import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
+
+
+class PageCounterModel extends ChangeNotifier {
+  int _currentPage = 0;
+  final PageController _controller = PageController(initialPage: 0);
+  int get currentPage => _currentPage;
+  PageController get controller => _controller;
+  void increment() {
+    _currentPage++;
+    notifyListeners();
+  }
+  void setPage(int page) {
+    _currentPage = page;
+    notifyListeners();
+  }
+}
+
+
+class ClassSelectModel extends ChangeNotifier { 
+  late Map<String, dynamic> _tpf;
+  List<List<String>> get selectedClasses => (_tpf["SelectedClasses"] as List).cast<List>().map((e) => e.cast<String>()).toList();
+  bool initialized = false;
+
+  Future<void> init() async { 
+    final directory = await getApplicationDocumentsDirectory();
+    final tempConfig = File('${directory.path}/${StaticsVar.tempConfigPath}');
+    if (!await tempConfig.exists()) {
+      await tempConfig.create(recursive: true);
+      await tempConfig.writeAsString(jsonEncode(StaticsVar.tempConfig));
+    } else {
+    _tpf = jsonDecode(await tempConfig.readAsString());
+    }
+    initialized = true;
+    notifyListeners();
+  }
+
+  Future<void> save() async { 
+    final directory = await getApplicationDocumentsDirectory();
+    final tempConfig = File('${directory.path}/${StaticsVar.tempConfigPath}');
+    await tempConfig.writeAsString(jsonEncode(_tpf));
+  }
+
+  void addClass(List<String> className){
+    if (!selectedClasses.any((e) => e[0] == className[0] && e[1] == className[1])) {
+      selectedClasses.add(className);
+      save();
+      notifyListeners(); 
+    }
+  }
+
+  void removeClass(List<String> className) {
+    selectedClasses.removeWhere((e) => e[0] == className[0] && e[1] == className[1]);
+    save();
+    notifyListeners(); 
+  }
+
+  void commitTempConfig(Map<String, dynamic> tpf) {
+    _tpf = tpf;
+    save();
+    notifyListeners();
+  }
+}
