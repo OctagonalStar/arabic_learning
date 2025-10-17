cc5c80664dbe1f8771333c6de4d08365480e161a
OctagonalStar <76486554+OctagonalStar@users.noreply.github.com>
2025-10-17 20:19:51 +0800

    add ICON
    Signed-off-by: OctagonalStar <76486554+OctagonalStar@users.noreply.github.com>

--- lib/statics_var.dart
+++ lib/statics_var.dart
@@ -3,7 +3,7 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 
 class StaticsVar {
-  static const String appName = 'arabic Learning';
+  static const String appName = 'ar学';
   static const String appVersion = '1.0.0';
   static const String tempConfigPath = 'arabicLearning/tpc.json';
   static const Map<String, dynamic> tempConfig = {"SelectedClasses":[]};
