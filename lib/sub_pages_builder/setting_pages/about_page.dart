2b7c08777e0c35788fd5dd6cb99961311ad63ab0
OctagonalStar <76486554+OctagonalStar@users.noreply.github.com>
2025-10-25 22:45:45 +0800

    change in app lic

--- lib/sub_pages_builder/setting_pages/about_page.dart
+++ lib/sub_pages_builder/setting_pages/about_page.dart
@@ -16,7 +16,7 @@ class AboutPage extends StatelessWidget {
       body: ListView(
         children: [
           TextContainer(text: "关于"),
-          TextContainer(text: "该软件仅供学习使用，请勿用于商业用途。\n该软件基于GNU GENERAL PUBLIC LICENSE (Version 3)协议开源，协议原文详见页面底部。", style: TextStyle(color: Colors.redAccent, fontWeight: FontWeight.bold)),
+          TextContainer(text: "该软件仅供学习使用，请勿用于商业用途。\n该软件基于GNU AFFERO GENERAL PUBLIC LICENSE (Version 3)协议开源，协议原文详见页面底部。", style: TextStyle(color: Colors.redAccent, fontWeight: FontWeight.bold)),
           TextContainer(text: "目前该软件仅由 OctagonalStar(别问为什么写网名) 一人开发（其实主要是为了学flutter框架写的），如果有什么问题或者提议都欢迎提issue（或者线下真实？）。\n该软件 <Ar 学>，主要是为了帮助大家掌握阿语词汇（毕竟上课词汇都要听晕了）"),
           TextContainer(text: "免责声明"),
           Container(
@@ -35,11 +35,11 @@ class AboutPage extends StatelessWidget {
                 Text("2. 该软件不会对你的阿拉伯语成绩做出任何担保，若你出现阿拉伯语成绩不理想的情况请先考虑自己的问题 :)"),
                 Text("3. 由于软件在不同系统上运行可能存在兼容性问题，软件出错造成的任何损失（包含精神损伤），软件作者和其他贡献者不会担负任何责任"),
                 Text("4. 你知晓并理解如果你错误地使用软件（如使用错误的数据集）造成的任何后果，乃至二次宇宙大爆炸，都需要你自行承担"),
-                Text("5. 其他在GNU GENERAL PUBLIC LICENSE (Version 3)开源协议下的条款"),
+                Text("5. 其他在GNU AFFERO GENERAL PUBLIC LICENSE (Version 3)开源协议下的条款"),
               ],
             ),
           ),
-          TextContainer(text: "GNU GENERAL PUBLIC LICENSE (Version 3)开源协议 / GNU GENERAL PUBLIC LICENSE License"),
+          TextContainer(text: "GNU AFFERO GENERAL PUBLIC LICENSE (Version 3)开源协议 / GNU AFFERO GENERAL PUBLIC LICENSE"),
           TextContainer(text: "Copyright (C) <2025>  <OctagonalStar>\n该软件通过GNU GENERAL PUBLIC LICENSE (Version 3)协议授权给 \"${setting["User"]}\"，协议内容详见下方："),
           TextContainer(text: LicenseVars.theAPPLICENSE)
         ],
