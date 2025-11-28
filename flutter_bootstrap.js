244acaab2c8e431017a4c7f3e513c9eefab5a582
OctagonalStar <OctagonalStar@users.noreply.github.com>
2025-11-28 15:52:54 +0000

    deploy: 19f0aac99f0388c2327c3497dba2ab21ad27b762

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"d3d45dcf251823c1769909cd43698d126db38d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2478535632"
+    serviceWorkerVersion: "2376061051"
   }
 });
