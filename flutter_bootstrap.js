902c9d79ca406c90ad760a47eb46a1386ab7ae87
OctagonalStar <OctagonalStar@users.noreply.github.com>
2025-12-12 12:18:42 +0000

    deploy: 83f7f4d0c4d9c2e1a0eef833247d20b5fc646c4f

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -41,11 +41,8 @@ const loading = document.createElement('div');
 document.body.appendChild(loading);
 loading.textContent = "正在加载中(Step 1)...\n若长时间无响应请尝试更换网络";
 _flutter.loader.load({
-    config: {
-        'canvasKitBaseUrl': '/arabic_learning/canvaskit/',
-    },
     serviceWorkerSettings: {
-        serviceWorkerVersion: "4259423632",
+        serviceWorkerVersion: "1800742616",
     },
     onEntrypointLoaded: async function (engineInitializer) {
         loading.textContent = "正在加载中(Step 2)...长时间无响应请尝试删除缓存或更换浏览器";
