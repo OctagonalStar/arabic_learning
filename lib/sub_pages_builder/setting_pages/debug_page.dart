b3543555005d6cded9ff4b3a4994c200dc2cb0ad
OctagonalStar <76486554+OctagonalStar@users.noreply.github.com>
2025-12-12 21:49:32 +0800

    feat(log): add log filter
    Signed-off-by: OctagonalStar <76486554+OctagonalStar@users.noreply.github.com>

--- lib/sub_pages_builder/setting_pages/debug_page.dart
+++ lib/sub_pages_builder/setting_pages/debug_page.dart
@@ -1,4 +1,5 @@
 import 'package:arabic_learning/funcs/ui.dart';
+import 'package:arabic_learning/vars/statics_var.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -22,18 +23,56 @@ class _DebugPage extends State<DebugPage> {
       body: Column(
         children: [
           TextContainer(text: "该页面为软件调试/测试和bug反馈使用，非必要请勿开启日志捕获，以免性能损耗", style: TextStyle(color: Colors.redAccent)),
-          Row(
-            children: [
-              Icon(Icons.logo_dev),
-              Expanded(child: Text("启用软件内日志捕获")),
-              Switch(
-                value: context.watch<Global>().settingData["Debug"], 
-                onChanged: (value){
-                  context.read<Global>().settingData["Debug"] = value;
-                  context.read<Global>().updateSetting();
-                }
-              )
-            ],
+          Container(
+            decoration: BoxDecoration(
+              borderRadius: StaticsVar.br,
+              color: Theme.of(context).colorScheme.onPrimary
+            ),
+            padding: EdgeInsets.only(left: 16.0, right: 16.0),
+            child: Row(
+              children: [
+                Icon(Icons.logo_dev),
+                Expanded(child: Text("启用软件内日志捕获")),
+                Switch(
+                  value: context.watch<Global>().settingData["Debug"]["internalLog"], 
+                  onChanged: (value){
+                    context.read<Global>().settingData["Debug"]["internalLog"] = value;
+                    context.read<Global>().updateSetting();
+                  }
+                )
+              ],
+            ),
+          ),
+          Container(
+            decoration: BoxDecoration(
+              borderRadius: StaticsVar.br,
+              color: Theme.of(context).colorScheme.onSecondary
+            ),
+            padding: EdgeInsets.only(left: 16.0, right: 16.0),
+            child: Row(
+              children: [
+                Icon(Icons.logo_dev_outlined),
+                Expanded(child: Text("日志等级")),
+                DropdownButton(
+                  items: [
+                    DropdownMenuItem(value: 0,child: Text("Level.ALL")),
+                    DropdownMenuItem(value: 1,child: Text("Level.FINEST")),
+                    DropdownMenuItem(value: 2,child: Text("Level.FINER")),
+                    DropdownMenuItem(value: 3,child: Text("Level.FINE")),
+                    DropdownMenuItem(value: 4,child: Text("Level.INFO")),
+                    DropdownMenuItem(value: 5,child: Text("Level.WARNING")),
+                    DropdownMenuItem(value: 6,child: Text("Level.SEVERE")),
+                    DropdownMenuItem(value: 7,child: Text("Level.SHOUT")),
+                    DropdownMenuItem(value: 8,child: Text("Level.OFF")),
+                  ], 
+                  value: context.watch<Global>().settingData["Debug"]["internalLevel"],
+                  onChanged: (value) {
+                    context.read<Global>().settingData["Debug"]["internalLevel"] = value;
+                    context.read<Global>().updateSetting();
+                  }
+                )
+              ],
+            ),
           ),
           SizedBox(
             width: MediaQuery.of(context).size.width,
