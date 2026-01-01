e54c028ef173f87d085d165aa5cde8aa8855a078
OctagonalStar <76486554+OctagonalStar@users.noreply.github.com>
2026-01-01 22:10:42 +0800

    feat(syncPage): add local import/export ui
    Signed-off-by: OctagonalStar <76486554+OctagonalStar@users.noreply.github.com>

--- lib/sub_pages_builder/setting_pages/sync_page.dart
+++ lib/sub_pages_builder/setting_pages/sync_page.dart
@@ -29,6 +29,7 @@ class _DataSyncPage extends State<DataSyncPage> {
       user: context.read<Global>().globalConfig.webSync.account.userName,
       password: context.read<Global>().globalConfig.webSync.account.passWord
     );
+    MediaQueryData mediaQuery = MediaQuery.of(context);
 
     return Scaffold(
       appBar: AppBar(
@@ -37,152 +38,191 @@ class _DataSyncPage extends State<DataSyncPage> {
       body: ListView(
         children: [
           TextContainer(text: "该功能还处在预览阶段", style: TextStyle(color: Colors.redAccent)),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
+          SettingItem(
+            title: "远程",
+            padding: EdgeInsets.all(8.0),
             children: [
-              SettingItem(
-                title: "远程",
-                padding: EdgeInsets.all(8.0),
+              Row(
                 children: [
-                  Row(
-                    children: [
-                      Icon(Icons.account_box, size: 36),
-                      Expanded(
-                        child: Text("WebDAV账户"),
-                      ),
-                      ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                          shape: RoundedRectangleBorder(borderRadius: StaticsVar.br)
-                        ),
-                        onPressed: () async {
-                          await popAccountSetting(context);
-                          setState(() {});
-                        }, 
-                        child: Text("绑定")
-                      ),
-                    ],
+                  Icon(Icons.account_box, size: 36),
+                  Expanded(
+                    child: Text("WebDAV账户"),
                   ),
-                  Row(
-                    children: [
-                      Text("联通性检查: "),
-                      if(context.read<Global>().globalConfig.webSync.account.uri.isEmpty) Text("未绑定", style: Theme.of(context).textTheme.labelSmall),
-                      FutureBuilder(
-                        future: WebDAV.test(
-                          context.read<Global>().globalConfig.webSync.account.uri, 
-                          context.read<Global>().globalConfig.webSync.account.userName,
-                          password: context.read<Global>().globalConfig.webSync.account.passWord
-                        ), 
-                        builder: (context, snapshot) {
-                          if(snapshot.hasError) {
-                            return Row(
-                              children: [
-                                Icon(Icons.circle, color: Colors.redAccent, size: 18),
-                                Text("在测试中遇到了未知的异常", style: TextStyle(fontSize: 8))
-                              ],
-                            );
-                          }
-                          if(snapshot.connectionState == ConnectionState.waiting) {
-                            return CircularProgressIndicator();
-                          }
-                          if(snapshot.hasData) {
-                            return Row(
-                              children: [
-                                Icon(Icons.circle, color: snapshot.data![1] ? Colors.greenAccent : snapshot.data![0] ? Colors.amber : Colors.redAccent, size: 18)
-                              ],
-                            );
-                          }
-                          return CircularProgressIndicator();
-                        },
-                      )
-                    ],
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(borderRadius: StaticsVar.br)
+                    ),
+                    onPressed: () async {
+                      await popAccountSetting(context);
+                      setState(() {});
+                    }, 
+                    child: Text("绑定")
+                  ),
+                ],
+              ),
+              Row(
+                children: [
+                  Text("联通性检查: "),
+                  if(context.read<Global>().globalConfig.webSync.account.uri.isEmpty) Text("未绑定", style: Theme.of(context).textTheme.labelSmall),
+                  FutureBuilder(
+                    future: WebDAV.test(
+                      context.read<Global>().globalConfig.webSync.account.uri, 
+                      context.read<Global>().globalConfig.webSync.account.userName,
+                      password: context.read<Global>().globalConfig.webSync.account.passWord
+                    ), 
+                    builder: (context, snapshot) {
+                      if(snapshot.hasError) {
+                        return Row(
+                          children: [
+                            Icon(Icons.circle, color: Colors.redAccent, size: 18),
+                            Text("在测试中遇到了未知的异常", style: TextStyle(fontSize: 8))
+                          ],
+                        );
+                      }
+                      if(snapshot.connectionState == ConnectionState.waiting) {
+                        return CircularProgressIndicator();
+                      }
+                      if(snapshot.hasData) {
+                        return Row(
+                          children: [
+                            Icon(Icons.circle, color: snapshot.data![1] ? Colors.greenAccent : snapshot.data![0] ? Colors.amber : Colors.redAccent, size: 18)
+                          ],
+                        );
+                      }
+                      return CircularProgressIndicator();
+                    },
                   )
-                ], 
+                ],
               ),
-              StatefulBuilder(
-                builder: (context, setLocalState) {
-                  return SettingItem(
-                    title: "同步",
-                    padding: EdgeInsets.all(8.0),
-                    children: [
-                      Row(
-                        children: [
-                          Expanded(
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text("上传数据"),
-                                Text("将本地配置上传到WebDAV服务器", style: TextStyle(color: Colors.grey, fontSize: 8.0))
-                              ],
-                            )
-                          ),
-                          isUploading 
-                          ? CircularProgressIndicator()
-                          :ElevatedButton(
-                            onPressed: () async {
-                              context.read<Global>().uiLogger.info("用户上传数据");
-                              setLocalState(() {
-                                isUploading = true;
-                              });
-                              try{
-                                if(!webdav.isReachable) await webdav.connect();
-                                if(context.mounted) await webdav.upload(context.read<Global>().prefs);
-                              } catch (e) {
-                                if(!context.mounted) return;
-                                alart(context, e.toString());
-                                setLocalState(() {isUploading = false;});
-                                return;
-                              } 
-                              setLocalState(() {isUploading = false;});
-                              if(!context.mounted) return;
-                              alart(context, "已上传");
-                            },
-                            child: Text("上传")
-                          )
-                        ],
-                      ),
-                      Row(
-                        children: [
-                          Expanded(
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text("恢复数据"),
-                                Text("从WebDAV服务器恢复配置", style: TextStyle(color: Colors.grey, fontSize: 8.0))
-                              ],
-                            )
-                          ),
-                          isDownloading 
-                          ? CircularProgressIndicator()
-                          : ElevatedButton(
-                            onPressed: () async {
-                              context.read<Global>().uiLogger.info("用户恢复数据");
-                              setLocalState(() {
-                                isDownloading = true;
-                              });
-                              try{
-                                if(!webdav.isReachable) await webdav.connect();
-                                if(context.mounted) await webdav.download(context.read<Global>().prefs);
-                                if(context.mounted) context.read<Global>().conveySetting();
-                              } catch (e) {
-                                if(!context.mounted) return;
-                                alart(context, e.toString());
-                                setLocalState(() {isDownloading = false;});
-                                return;
-                              } 
-                              if(!context.mounted) return;
-                              setLocalState(() {isDownloading = false;});
-                              alart(context, "已恢复\n部分设置可能需要软件重启后才能生效");
-                            },
-                            child: Text("恢复")
-                          )
-                        ],
-                      )
-                    ],
-                  );
-                }
+              Row(
+                children: [
+                  Icon(Icons.cloud_upload),
+                  SizedBox(width: mediaQuery.size.width * 0.01),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text("上传数据"),
+                        Text("将本地配置上传到WebDAV服务器", style: TextStyle(color: Colors.grey, fontSize: 8.0))
+                      ],
+                    )
+                  ),
+                  isUploading 
+                  ? CircularProgressIndicator()
+                  :ElevatedButton(
+                    onPressed: () async {
+                      context.read<Global>().uiLogger.info("用户上传数据");
+                      setState(() {
+                        isUploading = true;
+                      });
+                      try{
+                        if(!webdav.isReachable) await webdav.connect();
+                        if(context.mounted) await webdav.upload(context.read<Global>().prefs);
+                      } catch (e) {
+                        if(!context.mounted) return;
+                        alart(context, e.toString());
+                        setState(() {isUploading = false;});
+                        return;
+                      } 
+                      setState(() {isUploading = false;});
+                      if(!context.mounted) return;
+                      alart(context, "已上传");
+                    },
+                    child: Text("上传")
+                  )
+                ],
               ),
+              Row(
+                children: [
+                  Icon(Icons.cloud_download),
+                  SizedBox(width: mediaQuery.size.width * 0.01),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text("恢复数据"),
+                        Text("从WebDAV服务器恢复配置", style: TextStyle(color: Colors.grey, fontSize: 8.0))
+                      ],
+                    )
+                  ),
+                  isDownloading 
+                  ? CircularProgressIndicator()
+                  : ElevatedButton(
+                    onPressed: () async {
+                      context.read<Global>().uiLogger.info("用户恢复数据");
+                      setState(() {
+                        isDownloading = true;
+                      });
+                      try{
+                        if(!webdav.isReachable) await webdav.connect();
+                        if(context.mounted) await webdav.download(context.read<Global>().prefs);
+                        if(context.mounted) context.read<Global>().conveySetting();
+                      } catch (e) {
+                        if(!context.mounted) return;
+                        alart(context, e.toString());
+                        setState(() {isDownloading = false;});
+                        return;
+                      } 
+                      if(!context.mounted) return;
+                      setState(() {isDownloading = false;});
+                      alart(context, "已恢复\n部分设置可能需要软件重启后才能生效");
+                    },
+                    child: Text("恢复")
+                  )
+                ],
+              )
             ],
           ),
+          SettingItem(
+            title: "本地", 
+            padding: EdgeInsets.all(8.0),
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Icon(Icons.output),
+                  SizedBox(width: mediaQuery.size.width * 0.01),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text("导出数据"),
+                        Text("将当前软件数据作为文件导出", style: TextStyle(color: Colors.grey, fontSize: 8.0))
+                      ],
+                    ),
+                  ),
+                  ElevatedButton(
+                    onPressed: (){
+                      // TODO: export
+                    }, 
+                    child: Text("导出")
+                  )
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Icon(Icons.input),
+                  SizedBox(width: mediaQuery.size.width * 0.01),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text("导入数据"),
+                        Text("将文件中的配置覆盖软件配置", style: TextStyle(color: Colors.grey, fontSize: 8.0))
+                      ],
+                    ),
+                  ),
+                  ElevatedButton(
+                    onPressed: (){
+                      // TODO: import
+                    }, 
+                    child: Text("导入")
+                  )
+                ],
+              ),
+            ]
+          )
         ],
       ),
     );
