cd463876c7cdeb145e157bda691d0528b70b9dc7
OctagonalStar <76486554+OctagonalStar@users.noreply.github.com>
2026-08-23 17:51:51 +0800

    build: kotlin and gradle
    Signed-off-by: OctagonalStar <76486554+OctagonalStar@users.noreply.github.com>

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -19,8 +19,10 @@ android {
         targetCompatibility = JavaVersion.VERSION_17
     }
 
-    kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_17.toString()
+    kotlin {
+        compileOptions {
+            jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
+        }
     }
 
     defaultConfig {
