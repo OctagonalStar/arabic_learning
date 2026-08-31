dbd846c83aa156d6fc306be384c7ad9d1630839b
OctagonalStar <OctagonalStar@users.noreply.github.com>
2026-08-31 03:40:16 +0000

    deploy: 9cc1cbaf3b94300b4b5386f6f24f6153f72f3ad3

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -46,7 +46,7 @@ document.body.appendChild(loading);
 loading.textContent = "正在加载中(Step 1)...\n若长时间无响应请尝试更换网络";
 _flutter.loader.load({
     serviceWorkerSettings: {
-        serviceWorkerVersion: "838816820" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */,
+        serviceWorkerVersion: "3763698746" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */,
     },
     onEntrypointLoaded: async function (engineInitializer) {
         loading.textContent = "正在加载中(Step 2)...长时间无响应请尝试删除缓存或更换浏览器";
